10a87e2bee7bc76395f4469c90d9b949d4306df2
Ntokozo Majozi (Group) <ntokozo.majozi@global.ntt>
2021-02-13 18:19:42 +0200

    init commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MyCounterComponent } from './my-counter/my-counter.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'count', component: MyCounterComponent },
+  { path: '', redirectTo: 'count', pathMatch: 'full' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
